56eedcf6e0402c9457ecf6591d8fec78ecddf235
anuragsinghbhandari <anuoo3ups@gmail.com>
2025-12-25 01:17:05 +0530

    final format test

--- format_test.py
+++ format_test.py
@@ -1,6 +1,6 @@
 def add(a, b):
     """
-    this function adds two numbers and returns the result without validating input types and also demonstrates badly formatted code example in docstring
+    this function adds two numbers and returns the result without validating input types and also demonstrates badly formatted code example in docstring.
     Example:
         >>> add( 1,2 )
         3
