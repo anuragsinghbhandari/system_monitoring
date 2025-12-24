1c2a4f4d66fa435cde0e5e470ace32e2d50025e3
anuragsinghbhandari <anuoo3ups@gmail.com>
2025-12-25 00:56:10 +0530

    bad code to test formatting

---
+++ format_test.py
@@ -0,0 +1,11 @@
+import os,sys
+
+
+def add( a,b ):
+    """this function adds two numbers and returns the result without validating input types and also demonstrates badly formatted code example in docstring
+    Example:
+        >>> add( 1,2 )
+        3
+    """
+    return a+b
+
